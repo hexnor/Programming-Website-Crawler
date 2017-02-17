1017c9a95d77faec279f3b2152cd1279c2f88ed2
hexnor <cse.ykr@gmail.com>
2017-02-17 17:32:42 +0530

    Done  Linking for Codechef

--- src/programming/website/crawler/ProgrammingWebsiteCrawler.java
+++ src/programming/website/crawler/ProgrammingWebsiteCrawler.java
@@ -14,7 +14,7 @@ public class ProgrammingWebsiteCrawler {
         Scanner sc=new Scanner(System.in);
          try {
                  int p=0;
-                 String logd="############################################\n#### Programming Website Solution Crawler ####### \n############################################ ";
+                 String logd="############################################\n Programming Website Solution Crawler ###### \n############################################ ";
                  
                  while(p<logd.length())
                  {  Thread.sleep(10);
@@ -22,14 +22,27 @@ public class ProgrammingWebsiteCrawler {
                      p++;
                  }
 } catch(InterruptedException ex) {
-    Thread.currentThread().interrupt();
+    
 }
-         
-        System.out.println("Enter 1 For Codechef \nEnter 2 For Hackerrank \nEnter 3 For Spoj \n"); 
+         String username="yr96";
+        System.out.println("Enter 1 For Codechef \nEnter 2 For Hackerrank \nEnter 3 For Spoj");
+       
         int choice=sc.nextInt();
+         
         if(choice==1){
-            
-        }
+          
+            System.out.println("Enter the user id for codechef");
+        // username=sc.nextLine();
+            Codechef obj=new Codechef(username);
+            System.out.println("Now starting");
+            obj.start();
+            System.out.println("fininshed");
+        }else
+            if (choice==2){
+                new Hackerrack();
+            }else
+                if(choice==3)
+                    new Spoj();
          
     }
     
